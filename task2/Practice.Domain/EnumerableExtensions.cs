7ee41d65c750bbf915d26ab4a53689892788beca
Yakdanol <91282447+Yakdanol@users.noreply.github.com>
2023-07-08 19:42:26 +0300

    Add files via upload

---
+++ task2/Practice.Domain/EnumerableExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace Practice.Domain;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<IEnumerable<T>> 
+        GetCombinations<T>(
+            this IEnumerable<T> collection, 
+            int length, 
+            CollectionsComparer<T> comparer) // комбинации
+        where T : IComparable 
+    {
+        CheckCollections(collection, comparer);
+
+        if (length == 1) return collection.Select(t => new T[] { t });
+        return GetCombinations(collection, length - 1, comparer)
+            .SelectMany(t => collection.Where(o => o.CompareTo(t.Last()) >= 0), 
+                (t1, t2) => t1.Concat(new T[] { t2 }));
+    }
+    
+    public static IEnumerable<IEnumerable<T>> 
+        GetSubset<T>(
+            this IEnumerable<T> collection, 
+            CollectionsComparer<T> comparer) // подмножества
+    {
+        CheckCollections(collection, comparer);
+        
+        var set = new List<IEnumerable<T>>() { Enumerable.Empty<T>() };
+        return collection.Aggregate((IEnumerable<IEnumerable<T>>)set, 
+            (x, y) => x.Concat(x.Select(z 
+                => z.Concat(new List<T>() { y }))));
+    }
+    
+    public static IEnumerable<IEnumerable<T>>
+        GetPermutations<T>(
+            this IEnumerable<T> collection, 
+            int length, 
+            CollectionsComparer<T> comparer) // перестановки
+    {
+        CheckCollections(collection, comparer);
+
+        if (length == 1) return collection.Select(t => new T[] { t });
+        return GetPermutations(collection, length - 1, comparer)
+            .SelectMany(t => collection.Where(o => !t.Contains(o)),
+                (t1, t2) => t1.Concat(new T[] { t2 }));
+    }
+
+    public static void CheckCollections<T>(this IEnumerable<T> collection, CollectionsComparer<T> comparer)
+    {
+        if (collection == null || comparer == null)
+            throw new ArgumentNullException("Null аргумент в компораторе", nameof(collection));
+        
+        if (collection.Distinct(comparer).Count() != collection.Count())
+            throw new ArgumentException("Повтор значений в коллекции", nameof(collection));
+    }
+}
---
+++ task2/Practice.Launcher.App/Program.cs
@@ -0,0 +1,38 @@
+﻿using Practice.Domain;
+
+namespace Practice.Launcher.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                var test1 = new List<int>() { 1, 2 };
+                var test2 = new List<int>() { 1, 2, 3 };
+                var test3 = new List<int>() { 1, 2, 3, 4 };
+
+                Console.WriteLine("[" + string.Join(", ",
+                    test2.GetCombinations(2, CollectionsComparer<int>.Instance)
+                        .Select(item => "[" + string.Join(", ", item) + "]")) + "]");
+                Console.WriteLine("[" + string.Join(", ",
+                    test2.GetPermutations(test2.Count, CollectionsComparer<int>.Instance)
+                        .Select(item => "[" + string.Join(", ", item) + "]")) + "]");
+                Console.WriteLine("[" + string.Join(", ",
+                    test2.GetSubset(CollectionsComparer<int>.Instance)
+                        .Select(item => "[" + string.Join(", ", item) + "]")) + "]");
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
